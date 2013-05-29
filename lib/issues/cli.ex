5fe7b0298e6472884a5aea841168d588f97269e9
Kristoffer Roupé <kitofr@gmail.com>
2013-05-29 22:41:18 +0200

    Updated test env. Added tests

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -41,6 +41,25 @@ defmodule Issues.CLI do
 
   def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
+      |> decode_response
+      |> convert_to_list_of_hashdicts
+      |> sort_into_ascending_order
+  end
+
+  def decode_response({:ok, body}), do: Jsonex.decode(body)
+  def decode_response({:error, msg}) do
+    error = Jsonex.decode(msg)["message"]
+    IO.puts "Error fetching from Github: #{error}"
+    System.halt(2)
+  end
+
+  def convert_to_list_of_hashdicts(list) do
+    Enum.map(list, HashDict.new(&1))
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+              fn i1, i2 -> i1["created_at"] < i2["created_at"] end
   end
 end
 
